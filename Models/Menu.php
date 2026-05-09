fe258e9c09962412d6c80a30c94eed90cfca7239
Ovi <email_roviatunhasanah525@gmail.com>
2026-05-09 19:42:44 +0700

    Memasukkan kode menu

---
+++ Models/Menu.php
@@ -0,0 +1,47 @@
+<?php
+
+class Menu {
+
+    public function getMenu() {
+
+        return [
+
+            [
+                "nama" => "Croissant",
+                "harga" => "18K",
+                "gambar" => "croissant.jpg"
+            ],
+
+            [
+                "nama" => "Chocolate Cake",
+                "harga" => "35K",
+                "gambar" => "cake.jpg"
+            ],
+
+            [
+                "nama" => "Strawberry Tart",
+                "harga" => "28K",
+                "gambar" => "tart.jpg"
+            ],
+
+            [
+                "nama" => "Donut Glazed",
+                "harga" => "15K",
+                "gambar" => "donut.jpg"
+            ],
+
+            [
+                "nama" => "Cinnamon Roll",
+                "harga" => "22K",
+                "gambar" => "roll.jpg"
+            ],
+
+            [
+                "nama" => "Macaron Box",
+                "harga" => "40K",
+                "gambar" => "macaron.jpg"
+            ]
+
+        ];
+    }
+}
