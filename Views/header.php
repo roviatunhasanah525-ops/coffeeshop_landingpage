5eb6c3ae2e25b18be415cc0f6d7a16d8b4c5fcdc
Ovi <email_roviatunhasanah525@gmail.com>
2026-05-09 19:47:20 +0700

    manambahkan kode header

---
+++ Views/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Sweet Crumbs Bakery</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+</head>
+
+<body>
